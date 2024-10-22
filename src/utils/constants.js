d264f38cf4093b22939dc07898ebae333d52f346
sandeep <sandeeps16xgmail.com>
2024-10-22 21:47:42 +0530

    key secure

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,8 +1,6 @@
-export const Google_Api_Key = "AIzaSyADNt0LxouYLJNhCulcWTp3TpH6bf-8DwE";
-
 const Youtube_Video_Url =
   "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=" +
-  Google_Api_Key;
+  process.env.Google_Api_Key;
 
 export const Youtube_Search_Query_Api =
   "https://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
