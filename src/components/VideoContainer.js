4b0e152267e179ade023e3e767c5f7ef26778dfd
sandeep <sandeeps16xgmail.com>
2024-11-25 11:49:09 +0530

    name change

--- src/components/VideoContainer.js
+++ src/components/VideoContainer.js
@@ -20,7 +20,7 @@ const VideoContainer = () => {
   }, []);
 
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
       {/* Render VideoCard only if videos[0] exists */}
       {videos?.length > 0 ? (
         videos.map((video) => (
